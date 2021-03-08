c172b42ab536d6ec513d3ad131753fd75279f23a
Jan-Christoph Kuester <jckuester@gmail.com>
2021-03-08 19:23:52 +0100

    Upgrade to aws-sdk-v.1.2

--- aws/client_pool.go
+++ aws/client_pool.go
@@ -1,9 +1,10 @@
 package aws
 
 import (
+	"context"
 	"sync"
 
-	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 // clientPoolThreadSafe is a concurrent map implementation to store multiple AWS clients.
@@ -20,7 +21,7 @@ type ClientKey struct {
 // If profiles, regions, or both are empty, credentials and regions are picked up via the usual default provider chain,
 // respectively. For example, if regions are empty, the region is first looked for via the according region environment variable
 // or second the default region for each profile is used from `~/.aws/config`.
-func NewClientPool(profiles []string, regions []string) (map[ClientKey]Client, error) {
+func NewClientPool(ctx context.Context, profiles []string, regions []string) (map[ClientKey]Client, error) {
 	errors := make(chan error)
 	wgDone := make(chan bool)
 
@@ -40,8 +41,9 @@ func NewClientPool(profiles []string, regions []string) (map[ClientKey]Client, e
 					defer wg.Done()
 
 					client, err := NewClient(
-						external.WithSharedConfigProfile(p),
-						external.WithRegion(r))
+						ctx,
+						config.WithSharedConfigProfile(p),
+						config.WithRegion(r))
 					if err != nil {
 						errors <- err
 						return
@@ -60,7 +62,7 @@ func NewClientPool(profiles []string, regions []string) (map[ClientKey]Client, e
 			go func(p string) {
 				defer wg.Done()
 
-				client, err := NewClient(external.WithSharedConfigProfile(p))
+				client, err := NewClient(ctx, config.WithSharedConfigProfile(p))
 				if err != nil {
 					errors <- err
 					return
@@ -78,7 +80,7 @@ func NewClientPool(profiles []string, regions []string) (map[ClientKey]Client, e
 			go func(r string) {
 				defer wg.Done()
 
-				client, err := NewClient(external.WithRegion(r))
+				client, err := NewClient(ctx, config.WithRegion(r))
 				if err != nil {
 					errors <- err
 					return
@@ -90,7 +92,7 @@ func NewClientPool(profiles []string, regions []string) (map[ClientKey]Client, e
 			}(region)
 		}
 	} else {
-		client, err := NewClient()
+		client, err := NewClient(ctx)
 		if err != nil {
 			return nil, err
 		}
--- aws/client_pool_test.go
+++ aws/client_pool_test.go
@@ -1,10 +1,11 @@
 package aws_test
 
 import (
-	"github.com/jckuester/awsls/test"
-	"github.com/jckuester/awstools-lib/aws"
+	"context"
 	"testing"
 
+	"github.com/jckuester/awsls/test"
+	"github.com/jckuester/awstools-lib/aws"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -116,7 +117,7 @@ func TestNewAWSClientPool(t *testing.T) {
 			err = test.SetMultiEnvs(tt.envs)
 			require.NoError(t, err)
 
-			got, err := aws.NewClientPool(tt.args.profiles, tt.args.regions)
+			got, err := aws.NewClientPool(context.Background(), tt.args.profiles, tt.args.regions)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NewClientPool() error = %v, wantErr %v", err, tt.wantErr)
 				return
